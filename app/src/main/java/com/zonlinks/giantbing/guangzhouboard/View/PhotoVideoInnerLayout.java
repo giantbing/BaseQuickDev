871706985120aedfd8607bf5e3180e6bd47ed4c4
giantbing <giantbing@hotmail.com>
2017-09-04 18:59:59 +0800

    2017.9.4更新

--- app/src/main/java/com/zonlinks/giantbing/guangzhouboard/View/PhotoVideoInnerLayout.java
+++ app/src/main/java/com/zonlinks/giantbing/guangzhouboard/View/PhotoVideoInnerLayout.java
@@ -3,6 +3,7 @@ package com.zonlinks.giantbing.guangzhouboard.View;
 import android.content.Context;
 import android.os.Handler;
 import android.support.v4.view.ViewPager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,6 +11,9 @@ import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
 import com.bumptech.glide.Glide;
+import com.shuyu.gsyvideoplayer.listener.StandardVideoAllCallBack;
+import com.shuyu.gsyvideoplayer.model.GSYVideoModel;
+import com.shuyu.gsyvideoplayer.video.ListGSYVideoPlayer;
 import com.zonlinks.giantbing.guangzhouboard.Adapter.MainPagerAdapter;
 import com.zonlinks.giantbing.guangzhouboard.C;
 import com.zonlinks.giantbing.guangzhouboard.Entity.AllData;
@@ -31,33 +35,35 @@ public class PhotoVideoInnerLayout extends RelativeLayout {
 
     @BindView(R.id.videoinnerpager)
     ViewPager videoinnerpager;
+    @BindView(R.id.videoplayer)
+    ListGSYVideoPlayer videoplayer;
     private MainPagerAdapter mainPagerAdapter;
     private List<View> viewList;
     private ViewGroup viewGroup;
     private Context context;
-    private int currentIndex =0;
+    private int currentIndex = 0;
     private Handler handler;
     private MainPagerExcute ExcutLisnner;
     private int mainPosition;
-//    public PhotoInnerLayout(Context context, @Nullable AttributeSet attrs) {
+    //    public PhotoInnerLayout(Context context, @Nullable AttributeSet attrs) {
 //        super(context, attrs);
 //        this.context = context;
 //        initview();
 //       // splashContent.setText("2333333");
 //    }
-Runnable cycleRunnable = new Runnable() {
-    @Override
-    public void run() {
+    Runnable cycleRunnable = new Runnable() {
+        @Override
+        public void run() {
 
-            if (currentIndex>=viewList.size()){
+            if (currentIndex >= viewList.size()) {
                 currentIndex = 0;
             }
             videoinnerpager.setCurrentItem(currentIndex);
             currentIndex++;
             //递归循环，图片切换速度3秒一张
-        handler.postDelayed(this, 3000);
-    }
-};
+            handler.postDelayed(this, 3000);
+        }
+    };
 
     public int getMainPosition() {
         return mainPosition;
@@ -68,17 +74,20 @@ Runnable cycleRunnable = new Runnable() {
     }
 
 
-    public void startCycle(){
+    public void startCycle() {
         handler.post(cycleRunnable);
     }
-    public void stopCycle(){
+
+    public void stopCycle() {
         handler.removeCallbacks(cycleRunnable);
     }
-    public void freshView(){
+
+    public void freshView() {
         currentIndex = 0;
         videoinnerpager.setCurrentItem(currentIndex);
     }
-    public PhotoVideoInnerLayout(Context context, ViewGroup viewGroup, Handler handler,MainPagerExcute ExcutLisnner) {
+
+    public PhotoVideoInnerLayout(Context context, ViewGroup viewGroup, Handler handler, MainPagerExcute ExcutLisnner) {
         super(context);
         this.context = context;
         this.viewGroup = viewGroup;
@@ -88,24 +97,26 @@ Runnable cycleRunnable = new Runnable() {
         initview();
     }
 
-    public void loadData(List<AllData.SchoolCultureWallListBean> SchoolCultureWallList, List<AllData.SchoolVideoCultureWallListBean> videoCultureWallListBeen,int position){
-        if (position==0)
+    public void loadData(List<AllData.SchoolCultureWallListBean> SchoolCultureWallList, List<AllData.SchoolVideoCultureWallListBean> videoCultureWallListBeen, int position) {
+        if (position == 0)
             startCycle();
         else
             stopCycle();
         mainPosition = position;
         viewList.clear();
-        for (AllData.SchoolCultureWallListBean cultureWallListBean :SchoolCultureWallList){
+        for (AllData.SchoolCultureWallListBean cultureWallListBean : SchoolCultureWallList) {
             ImageView imageView = new ImageView(context);
-            Glide.with(context).load(C.BASEURL+cultureWallListBean.getImagePath())
+            Glide.with(context).load(C.BASEURL + cultureWallListBean.getImagePath())
                     .error(R.mipmap.image2)
                     .into(imageView);
             viewList.add(imageView);
 
         }
+        startPlay(videoCultureWallListBeen);
         mainPagerAdapter = new MainPagerAdapter(viewList);
         videoinnerpager.setAdapter(mainPagerAdapter);
     }
+
     private void initview() {
         //加载视图的布局
         View view = LayoutInflater.from(context).inflate(R.layout.pager_photovideoinner, viewGroup, false);
@@ -122,16 +133,8 @@ Runnable cycleRunnable = new Runnable() {
 
             @Override
             public void onPageSelected(int position) {
-                if (position==viewList.size()-1){
-                    ToastHelper.success(context,"woyaoxiayiyela!");
-                    handler.postDelayed(new Runnable() {
-                        @Override
-                        public void run() {
-                            ExcutLisnner.toNextPage(mainPosition+1);
-                            freshView();
-                            stopCycle();
-                        }
-                    },3000);
+                if (position == viewList.size() - 1) {
+
 
                 }
             }
@@ -145,5 +148,134 @@ Runnable cycleRunnable = new Runnable() {
     /**
      * 此方法会在所有的控件都从xml文件中加载完成后调用
      */
+    public void startPlay(List<AllData.SchoolVideoCultureWallListBean> videoCultureWallListBeen){
+        List<GSYVideoModel> videolist = new ArrayList<>();
+        for(AllData.SchoolVideoCultureWallListBean bean:videoCultureWallListBeen){
+            GSYVideoModel gsyVideoModel = new GSYVideoModel(bean.getImagePath(),bean.getTitle());
+            videolist.add(gsyVideoModel);
+            Log.d("2333", "startPlay: "+bean.getImagePath());
+        }
+        if (videolist.size()>0){
+            videoplayer.setUp(videolist,true,0);
+            videoplayer.getBackButton().setVisibility(View.GONE);
+            videoplayer.setIsTouchWiget(true);
+            videoplayer.startPlayLogic();
+            videoplayer.setStandardVideoAllCallBack(new StandardVideoAllCallBack() {
+                @Override
+                public void onPrepared(String url, Object... objects) {
 
+                }
+
+                @Override
+                public void onClickStartIcon(String url, Object... objects) {
+
+                }
+
+                @Override
+                public void onClickStartError(String url, Object... objects) {
+
+                }
+
+                @Override
+                public void onClickStop(String url, Object... objects) {
+
+                }
+
+                @Override
+                public void onClickStopFullscreen(String url, Object... objects) {
+
+                }
+
+                @Override
+                public void onClickResume(String url, Object... objects) {
+
+                }
+
+                @Override
+                public void onClickResumeFullscreen(String url, Object... objects) {
+
+                }
+
+                @Override
+                public void onClickSeekbar(String url, Object... objects) {
+
+                }
+
+                @Override
+                public void onClickSeekbarFullscreen(String url, Object... objects) {
+
+                }
+
+                @Override
+                public void onAutoComplete(String url, Object... objects) {
+                    // TODO: 2017/9/4
+                    ToastHelper.success(context, "woyaoxiayiyela!");
+                    handler.postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            ExcutLisnner.toNextPage(mainPosition + 1);
+                            freshView();
+                            stopCycle();
+                        }
+                    }, 3000);
+                }
+
+                @Override
+                public void onEnterFullscreen(String url, Object... objects) {
+
+                }
+
+                @Override
+                public void onQuitFullscreen(String url, Object... objects) {
+
+                }
+
+                @Override
+                public void onQuitSmallWidget(String url, Object... objects) {
+
+                }
+
+                @Override
+                public void onEnterSmallWidget(String url, Object... objects) {
+
+                }
+
+                @Override
+                public void onTouchScreenSeekVolume(String url, Object... objects) {
+
+                }
+
+                @Override
+                public void onTouchScreenSeekPosition(String url, Object... objects) {
+
+                }
+
+                @Override
+                public void onTouchScreenSeekLight(String url, Object... objects) {
+
+                }
+
+                @Override
+                public void onPlayError(String url, Object... objects) {
+                   // gsyVideoPlayer.setVisibility(View.GONE);
+                }
+
+                @Override
+                public void onClickStartThumb(String url, Object... objects) {
+
+                }
+
+                @Override
+                public void onClickBlank(String url, Object... objects) {
+
+                }
+
+                @Override
+                public void onClickBlankFullscreen(String url, Object... objects) {
+
+                }
+            });
+        }
+
+    }
 }
